e7973076ff3e2317e437b6dda1459d7a95b84723
David Conley <dconley1989@gmail.com>
2022-06-15 15:27:56 -0600

    practiced more typscript examples

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+let ageornone: number | string;
+
+function printName(name: string) {
+  console.log(name);
+}
+
+let changeName: (name: String) => void;
+
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person = {
+  name: "Dave",
+};
+
+let lotsOfPeople: Person[];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
