cc202ebd0b9c721f8a6d1bfdcd4ef2cb4c408d3e
David Conley <dconley1989@gmail.com>
2022-06-16 13:46:02 -0600

    created helper function for the onchange attribute of the input on the form and learned how to correctly add the typescript so that the event input is correctly described as to what is the input of the handleChange helper function

--- client/src/Components/ToDo.tsx
+++ client/src/Components/ToDo.tsx
@@ -1,13 +1,23 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 
 const ToDo: React.FC = () => {
   const [task, setTask] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setTask(e.currentTarget.value);
+  };
   return (
     <div>
       <h1>Todo</h1>
       <form>
         <label>
-          <input type="input" placeholder="Add a Task" value={task}></input>
+          <input
+            onChange={handleChange}
+            type="input"
+            placeholder="Add a Task"
+            value={task}
+          ></input>
         </label>
         <button type="submit">Add</button>
       </form>
