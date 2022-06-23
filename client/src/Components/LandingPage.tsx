e3b807e278e393ec7dec0bcd2b0616ec93a42c30
David Conley <dconley1989@gmail.com>
2022-06-23 15:02:07 -0600

    added some basic styling to the landing page component and got the background image to cover the main section like I wanted

--- client/src/Components/LandingPage.tsx
+++ client/src/Components/LandingPage.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import backgroundImg from "../Assets/austin-distel-7bMdiIqz_J4-unsplash.jpg";
+
+const StyledHeader = styled.header`
+  padding: 1rem;
+  background: #4682b4;
+  color: #faf0e6;
+  font-size: 1.2rem;
+`;
 
 const StyledMainDiv = styled.div`
-  background-image: url("../Assets/humberto-chavez-FVh_yqLR9eA-unsplash.jpg");
+  padding: 20rem 15rem;
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const LandingPageHook = styled.h2`
+  font-size: 2rem;
 `;
 
 const LandingPage = () => {
   return (
     <div>
-      <header></header>
+      <StyledHeader>
+        <h2>ConleyCare</h2>
+      </StyledHeader>
       <StyledMainDiv>
-        <h2>Tired of back Pain?</h2>
-        <p>Some statistic</p>
+        <LandingPageHook>Tired of back pain?</LandingPageHook>
+        <p>You're in good company too. 1 in 3 Adults experience back pain!</p>
         <button>Want a doctors recomendation?</button>
       </StyledMainDiv>
     </div>
