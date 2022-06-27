07014f9f18c611966acd6e9a322f8b737ae860d8
David Conley <dconley1989@gmail.com>
2022-06-27 10:05:55 -0600

    set up the reduxt tool kit store for the app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Components/App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
