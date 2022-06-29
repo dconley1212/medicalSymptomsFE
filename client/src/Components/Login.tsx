b0ac6480ebd27b4931517fe7ca122007c93372fa
David Conley <dconley1989@gmail.com>
2022-06-29 17:00:05 -0600

    added more css for the login component

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,5 +1,42 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  height: 100vh;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 5rem;
+  padding: 5rem;
+  background-color: #ffffff;
+  border-style: solid;
+  border-width: 5px;
+  border-image: linear-gradient(#4682b4, #db7093) 5;
+`;
+
+const StyledTitleLogo = styled.h2`
+  position: relative;
+  bottom: 2rem;
+  font-size: 2rem;
+  color: #4682b4;
+`;
+
+const StyledDirections = styled.h3`
+  display: flex;
+  justify-content: center;
+  font-size: 1rem;
+  margin-bottom: 1.5rem;
+`;
+const StyledInput = styled.input`
+  display: flex;
+`;
 
 const Login = () => {
   const [login, setLogin] = useState<{ [x: string]: string }>({
@@ -18,10 +55,10 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>ConleyCare</h2>
-        <h3>Please Sign In</h3>
+    <LoginWrapper>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledTitleLogo>ConleyCare</StyledTitleLogo>
+        <StyledDirections>Please Sign In</StyledDirections>
         <input
           placeholder="email"
           name="email"
@@ -35,8 +72,8 @@ const Login = () => {
           onChange={handleChange}
         />
         <button></button>
-      </form>
-    </div>
+      </StyledForm>
+    </LoginWrapper>
   );
 };
 
