34a7d1edaf34440e386eedf4f23a525c034f5219
David Conley <dconley1989@gmail.com>
2022-07-18 18:10:33 -0600

    I have one last problem to figure out and that is how to render only the name of the item in the cart once while increasing the quantity

--- client/src/features/Checkout/CheckoutItems.tsx
+++ client/src/features/Checkout/CheckoutItems.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAppSelector } from "../../app/hooks";
 
 const CheckoutItems = () => {
   const cartProducts = useAppSelector((state) => state);
+  const [sprayItem, setSprayItem] = useState<boolean>(false);
+  const [pillsItem, setPillsItem] = useState<boolean>(false);
+
+  useEffect(() => {
+    for (let i = 0; i < cartProducts.itemsInCart.cartItems.length; i++) {
+      if (cartProducts.itemsInCart.cartItems[i].id === 1) {
+        setSprayItem(true);
+      }
+      if (cartProducts.itemsInCart.cartItems[i].id === 2) {
+        setPillsItem(true);
+      }
+    }
+  }, [cartProducts.itemsInCart.cartItems]);
+
   return (
     <div>
       <h3>Cart</h3>
-      {cartProducts.itemsInCart.cartItems.map((item) => {
+      {/* {cartProducts.itemsInCart.cartItems.map((item) => {
         return (
           <div>
             <p>{item.name}</p>
@@ -15,10 +29,10 @@ const CheckoutItems = () => {
               <p>{cartProducts.itemsInCart.itemOne}</p>
             ) : (
               <p>{cartProducts.itemsInCart.itemTwo}</p>
-            )}
-          </div>
+            )} */}
+      {/* </div>
         );
-      })}
+      })} */}
       <p>Total Quantity: {cartProducts.itemsInCart.totalQuantity}</p>
       <p>Total Price: {cartProducts.itemsInCart.totalPrice}</p>
     </div>
