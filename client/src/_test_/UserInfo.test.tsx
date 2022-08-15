2021396bbc753e50d60e7c1fa4d5860bded3c525
David Conley <dconley1989@gmail.com>
2022-08-15 17:17:42 -0600

    tried to add some changes to update the testing module to the new react testing user-event but am running into problems

--- client/src/_test_/UserInfo.test.tsx
+++ client/src/_test_/UserInfo.test.tsx
@@ -1,9 +1,14 @@
 import UserInfo from "../features/UserAccount/UserInfo";
 import { Provider } from "react-redux";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { setup } from "@testing-library/user-event";
 import { store } from "../app/store";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "@testing-library/jest-dom";
+
+const user = setup({
+  skipPointerEventsCheck: true,
+  keyboardMap: [{ key: '"', code: "Digit2", shiftKey: true }],
+});
 
 describe("testing the UserInfo component", () => {
   test("the user info component renders", () => {
