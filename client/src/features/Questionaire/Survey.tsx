093f44ab2cf090fc12e6b25695e6f71f27c8887a
David Conley <dconley1989@gmail.com>
2022-09-12 14:21:07 -0600

    added a helper function get handle the onChange event for the select html file and added more details to the html input elments and select and option elements to update the page dynamically based on the users response

--- client/src/features/Questionaire/Survey.tsx
+++ client/src/features/Questionaire/Survey.tsx
@@ -28,28 +28,40 @@ const Survey = () => {
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setBackInfo({ ...backInfo, [e.target.name]: e.target.value });
   };
+  const handleDropDownChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setBackInfo({
+      ...backInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div>
       <form>
         <label>What is your height?</label>
         <input
+          name="height"
           onChange={handleInputChange}
           type="text"
           value={backInfo.height}
         />
         <label>What is your weight?</label>
         <input
+          name="weight"
           onChange={handleInputChange}
           type="text"
           value={backInfo.weight}
         />
         <label>Where is the back pain located?</label>
-        <select>
-          <option>upper left region</option>
-          <option> upper right region</option>
-          <option>middle region</option>
-          <option>lower left region</option>
-          <option>lower right region</option>
+        <select
+          name="painLocation"
+          onChange={handleDropDownChange}
+          value={backInfo.painLocation}
+        >
+          <option value="upper left region">upper left region</option>
+          <option value="upper right region"> upper right region</option>
+          <option value="middle region">middle region</option>
+          <option value="lower left region">lower left region</option>
+          <option value="lower right region">lower right region</option>
         </select>
         <label>How often is the pain occuring?</label>
         <input />
