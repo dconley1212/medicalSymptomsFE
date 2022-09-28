3bb152b6ade5d44e981beb35cc9251fcc3570ae4
David Conley <dconley1989@gmail.com>
2022-09-28 17:17:59 -0600

    my first test in the reviews component was not passing so I made some changes to get the first test to pass

--- client/src/_test_/Reviews/Reviews.test.tsx
+++ client/src/_test_/Reviews/Reviews.test.tsx
@@ -2,21 +2,22 @@ import Reviews from "../../features/Reviews/Reviews";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 describe("testing the Review component", () => {
   test("the document title is on the page", () => {
     render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/reviews">
-            <Reviews />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <Provider store={store}>
+          <Reviews />
+        </Provider>
+      </Router>
     );
 
-    const title = screen.getByText("What are people saying about our products");
+    const title = screen.getByText(
+      "What are people saying about our products?"
+    );
     expect(title).toBeInTheDocument();
   });
 });
