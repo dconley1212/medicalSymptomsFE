5fbdcd0bd4129b98855413d4a98a5420f031ed1b
David Conley <dconley1989@gmail.com>
2022-10-04 14:28:45 -0600

    added another test file for the editUserPaymentInfo component and test that all the elements that should be showing are rendering

---
+++ client/src/_test_/UserAccount_Test_/EditUserPaymentInfo.test.tsx
@@ -0,0 +1,29 @@
+import EditUserPaymentInfo from "../../features/UserAccount/EditUserPaymentInfo";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../app/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+describe("testing the EditUserPaymentInfo component", () => {
+  test("make sure every element is rendering that is supposed to be on the screen", () => {
+    render(
+      <Router>
+        <Provider store={store}>
+          <EditUserPaymentInfo />
+        </Provider>
+      </Router>
+    );
+    const cardNumber = screen.getByPlaceholderText("Card Number");
+    const nameOnCard = screen.getByPlaceholderText("Name on Card");
+    const expirationDate = screen.getByPlaceholderText("Expiration date");
+    const securityCode = screen.getByPlaceholderText("Security Code");
+    const addPayment = screen.getByRole("button", { name: "Add Payment" });
+
+    expect(cardNumber).toBeInTheDocument();
+    expect(nameOnCard).toBeInTheDocument();
+    expect(expirationDate).toBeInTheDocument();
+    expect(securityCode).toBeInTheDocument();
+    expect(addPayment).toBeInTheDocument();
+  });
+});
