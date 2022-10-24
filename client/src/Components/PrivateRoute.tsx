56e7704a414cd58177e10905f3ad82a350c9b433
David Conley <dconley1989@gmail.com>
2022-10-24 16:30:45 -0600

    started creating the pPrivateRoute component and the type for the props it will recieve

---
+++ client/src/Components/PrivateRoute.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type PrivateRouteProps = {
+  isAuthenticated: boolean;
+  path: string;
+  outlet: JSX.Element;
+};
+
+const PrivateRoute = () => {};
+
+export default PrivateRoute;
